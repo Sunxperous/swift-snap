3069f5f16842dd1752032ea1b2308fc3c252a57d
Sun Wang Jun <sunxperous@gmail.com>
2020-09-05 22:22:13 -0700

    Add shortcut to switch windows between displays

--- src/background.js
+++ src/background.js
@@ -1,5 +1,10 @@
 import "chrome-extension-async";
-import { determineScreenOfWindow, snap } from "./snap.js";
+import {
+  determineScreenOfWindow,
+  isScreenInDisplay,
+  snap,
+  snapToScreen,
+} from "./snap.js";
 import { Rect } from "./rect.js";
 
 const browser = chrome;
@@ -7,8 +12,48 @@ const browser = chrome;
 browser.commands.onCommand.addListener((command) => {
   browser.system.display.getInfo(async (displays) => {
     const currWindow = await browser.windows.getCurrent();
-    const screen = determineScreenOfWindow(currWindow, displays);
     const layouts = (await browser.storage.local.get()).saved;
+    const screen = determineScreenOfWindow(currWindow, displays);
+
+    if (command === "switch-next-display") {
+      const currentLayout = layouts.filter((layout) =>
+        Rect.calculateWindow(layout, screen).equals(currWindow)
+      );
+      const nextScreenIndex =
+        (displays.findIndex((display) =>
+          isScreenInDisplay(currWindow, display)
+        ) +
+          1) %
+        displays.length;
+      if (currentLayout.length > 0) {
+        snapToScreen(currentLayout[0], displays[nextScreenIndex]);
+      } else {
+        const topDiff = currWindow.top - screen.top;
+        const leftDiff = currWindow.left - screen.left;
+        const newDisplay = displays[nextScreenIndex];
+        if (
+          topDiff <= newDisplay.workArea.height &&
+          leftDiff <= newDisplay.workArea.width &&
+          currWindow.width <= newDisplay.workArea.width &&
+          currWindow.height <= newDisplay.workArea.height
+        ) {
+          const newWindow = new Rect(
+            newDisplay.workArea.top + topDiff,
+            newDisplay.workArea.left + leftDiff,
+            currWindow.width,
+            currWindow.height
+          );
+          await browser.windows.update(currWindow.id, newWindow);
+        } else {
+          snapToScreen(
+            Rect.forRatio(Rect.fromObj(currWindow), Rect.fromObj(screen)),
+            displays[nextScreenIndex]
+          );
+        }
+        return;
+      }
+    }
+
     let firstMatch = -1;
     let matchesCurrentLayout = false;
     for (let i = 0; i < layouts.length; ++i) {
--- src/snap.js
+++ src/snap.js
@@ -4,38 +4,51 @@ import { Rect } from "./rect.js";
 
 const browser = chrome;
 
-function determineScreenOfWindow(currWindow, displays) {
+function isScreenInDisplay(currWindow, display) {
   const windowCenterX = currWindow.left + currWindow.width / 2;
   const windowCenterY = currWindow.top + currWindow.height / 2;
+  const displayLeft = display.workArea.left;
+  const displayRight = display.workArea.left + display.workArea.width;
+  const displayTop = display.workArea.top;
+  const displayBottom = display.workArea.top + display.workArea.height;
+  return (
+    windowCenterX >= displayLeft &&
+    windowCenterX <= displayRight &&
+    windowCenterY >= displayTop &&
+    windowCenterY <= displayBottom
+  );
+}
+
+function displayOf(currWindow, displays) {
   for (let display of displays) {
-    const displayLeft = display.workArea.left;
-    const displayRight = display.workArea.left + display.workArea.width;
-    const displayTop = display.workArea.top;
-    const displayBottom = display.workArea.top + display.workArea.height;
-    if (
-      windowCenterX >= displayLeft &&
-      windowCenterX <= displayRight &&
-      windowCenterY >= displayTop &&
-      windowCenterY <= displayBottom
-    ) {
-      return display.workArea;
+    if (isScreenInDisplay(currWindow, display)) {
+      return display;
     }
   }
-  return display[0].workArea;
+  return displays[0];
+}
+
+function determineScreenOfWindow(currWindow, displays) {
+  return displayOf(currWindow, displays).workArea;
 }
 
 function snap(layout, callback) {
   browser.system.display.getInfo(async (displays) => {
     const currWindow = await browser.windows.getCurrent();
-    const screen = determineScreenOfWindow(currWindow, displays);
-    const newWindowSize = Rect.calculateWindow(layout, screen);
-    newWindowSize.state = "normal";
-    await browser.windows.update(currWindow.id, newWindowSize);
-    const port = browser.runtime.connect({name: 'snap-shortcut'});
-    port.postMessage("snap");
-    port.disconnect();
-    if (callback) callback();
+    const display = displayOf(currWindow, displays);
+    await snapToScreen(layout, display, callback);
   });
 }
 
-export { determineScreenOfWindow, snap };
+async function snapToScreen(layout, display, callback) {
+  const currWindow = await browser.windows.getCurrent();
+  const newWindowSize = Rect.calculateWindow(layout, display.workArea);
+  newWindowSize.state = "normal";
+  await browser.windows.update(currWindow.id, newWindowSize);
+  const port = browser.runtime.connect({ name: "snap-shortcut" });
+  port.postMessage("snap");
+  port.disconnect();
+  if (callback) callback();
+}
+
+export { determineScreenOfWindow, isScreenInDisplay, snap, snapToScreen };
